586a4a8d9926a183f5b50ec8232f7dfeeda191f6
quinnzee <quinnzee88@gmail.com>
2025-04-12 13:45:21 +0700

    learning about constant

---
+++ golang-training1/constant&operator/1constant.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	const full_name string = "Medicine"
+
+	fmt.Printf("Hello %s", full_name)
+
+}
--- golang-training1/constantoperator/1constant.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	const full_name string = "Kadal Terbang"
-
-	fmt.Printf("Hello %T", full_name)
-}
