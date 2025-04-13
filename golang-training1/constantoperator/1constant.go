b95ffc9a39916f7a98c782162b8b6d732952182c
quinnzee <quinnzee88@gmail.com>
2025-04-13 13:33:45 +0700

    learning Operators Arithmetic

--- golang-training1/constantoperator/1constant.go
+++ golang-training1/constantoperator/1constant.go
@@ -1,11 +1,10 @@
 package main
 
-import "fmt"
-
-func main() {
+/*func main() {
 	const full_name string = "Medicine"
 	//const (constant) in here meaning that everything inside is absolute
 
 	fmt.Printf("Hello %s", full_name)
 
 }
+*/
---
+++ golang-training1/constantoperator/2OperatorsArithmetic.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var value = 8 + 8*2
+	fmt.Println(value)
+}
