8f1999e528dae7882c49cdce10ac1e831394daf1
quinnzee <quinnzee88@gmail.com>
2025-04-14 23:20:07 +0700

    learning logical operators

---
+++ golang-training1/constantoperator/4logicalOperators.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b bool = true, false
+
+	//logical AND (&&)
+	fmt.Println(a && b)
+
+	//logical OR (||)
+	fmt.Println(a || b)
+
+	//logical NOT (!)
+	fmt.Println(!a)
+
+}
