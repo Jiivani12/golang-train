6a126710599149f07153b902ce19297a1bff2cc4
quinnzee <quinnzee88@gmail.com>
2025-04-14 23:09:21 +0700

    learning Relational Operations

---
+++ golang-training1/constantoperator/3relationalOperators.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int = 10, 5
+
+	//equal to
+	fmt.Println(a == b)
+
+	//not equal to
+	fmt.Println(a != b)
+
+	//greater than
+	fmt.Println(a > b)
+
+	//less than
+	fmt.Println(a < b)
+
+	//greater than or equal to
+	fmt.Println(a >= b)
+
+	//less than or equal to
+	fmt.Println(a <= b)
+}
