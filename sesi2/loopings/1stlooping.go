719c2a0c597a18297a819fc81ab4f003739fc7ec
quinnzee <quinnzee88@gmail.com>
2025-04-16 12:49:05 +0700

    learning about break n continue

--- sesi2/loopings/1stlooping.go
+++ sesi2/loopings/1stlooping.go
@@ -3,7 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 3; I++ { //DO NOT TRY THE INFINITE LOOP IT WAS DISASTER LMAOO
+	//for i := 0; i < 3; I++ { //DO NOT TRY THE INFINITE LOOP IT WAS DISASTER LMAOO
 		fmt.Println("angka", i)
+	}
+
+	//second way of looping
+	var i = 0
+
+	for i < 3 {
+		fmt.Println("Angka", i)
+		i++
+	}
+
+	//third way of looping
+	var i = 0
+
+	for {
+		fmt.Println("Angka", i)
+
+		i++
+		if i == 8 {
+			break
+		}
 	}
 }
