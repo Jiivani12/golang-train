4f8748ebd23c4b4fb70f957235908d7387393cb5
quinnzee <quinnzee88@gmail.com>
2025-04-20 13:27:33 +0700

    learning combining slice

--- sesi2/slice/5combineFunction.go
+++ sesi2/slice/5combineFunction.go
@@ -9,4 +9,10 @@ func main() {
 	fruits1 = append(fruits1[:3], "rambutan")
 
 	fmt.Printf("%#v\n", fruits1)
+
+	//kalau mau menambahkan index rambutan diantara index lainnya pake yg bawah
+	index := 3
+	fruits1 = append(fruits1[:index], append([]string{"rambutan"}, fruits1[index:]...)...)
+
+	fmt.Printf("%#v\n", fruits1)
 }
