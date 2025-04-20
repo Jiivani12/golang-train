d9e7290853c94a95779f8d4779491dce64e298f8
quinnzee <quinnzee88@gmail.com>
2025-04-20 13:08:21 +0700

    learning slice

---
+++ sesi2/slice/5combineFunction.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var fruits1 = []string{"apple", "banana", "mango", "durian", "pineapple"}
+
+	fruits1 = append(fruits1[:3], "rambutan")
+
+	fmt.Printf("%#v\n", fruits1)
+}
