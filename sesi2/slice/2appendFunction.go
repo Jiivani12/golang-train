63ddbf53b5c5beb82d8f1e857f565b798d10257f
quinnzee <quinnzee88@gmail.com>
2025-04-20 12:16:50 +0700

    learning about slice

---
+++ sesi2/slice/2appendFunction.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var fruits = make([]string, 3)
+	//_ = fruits
+
+	//fruits[0] = "apple"
+	//fruits[1] = "banana"
+	//fruits[2] = "mango"
+
+	//fmt.Printf("%#v", fruits)
+
+	//append function2
+
+	//var fruits = make([]string, 3)
+
+	//fruits = append(fruits, "apple", "banana", "mango")
+
+	//fmt.Printf("%#v", fruits)
+
+	//append function with ellipsis
+	var fruits1 = []string("apple", "banana", "mango")
+
+	var fruits2 = []string("durian", "pineapple", "starfruit")
+
+	fruits1 = append(fruits1, fruits2...)
+
+	fmt.Printf("%#v", fruits1)
+
+}
---
+++ sesi2/slice/3copyFunction.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var fruits1 = []string("apple", "banana", "mango")
+
+	var fruits2 = []string("durian", "pineapple", "starfruit")
+
+	nn := copy(fruits1, fruits2)
+
+	fmt.Println("Fruits1 =>", fruits1)
+	fmt.Println("Fruits2 =>", fruits2)
+	fmt.Println("Copied elements =>", nn)
+}
